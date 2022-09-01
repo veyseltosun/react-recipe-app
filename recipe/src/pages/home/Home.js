72a3a1d153c77b3a222fcd633418c58827189e1c
veyseltosun <postalarim1923@gmail.com>
2022-09-01 15:34:33 +0300

    recipe comp

--- recipe/src/pages/home/Home.js
+++ recipe/src/pages/home/Home.js
@@ -1,7 +1,8 @@
 import React, {useEffect, useState}from 'react'
 import Header from '../../components/header/Header'
 import axios from "axios";
-import { MainContainer, RecipeCard } from './HomeStyle';
+import { MainContainer} from './HomeStyle';
+import RecipeCardComp from './RecipeCardComp';
 
 
 const mealTypes = ["Breakfast", "Lunch", "Dinner", "Snack", "Teatime"];
---
+++ recipe/src/pages/home/RecipeCardComp.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function RecipeCardComp({recipe}) {
+    console.log("recipe", recipe)
+  return (
+    <div>
+        <h2>RecipeCard</h2>
+    </div>
+  )
+}
+
+export default RecipeCardComp
