ad21232d0fe36e00d1263689bafdd69944d8d1b4
veyseltosun <postalarim1923@gmail.com>
2022-09-01 18:08:42 +0300

    login page

---
+++ recipe/src/pages/login/Login.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { useNavigate} from "react-router-dom";
+import {
+  LoginContainer,
+  FormContainer,
+  Header,
+  StyledInput,
+  StyledForm,
+  StyledButton,
+  StyledImg,
+} from "./LoginStyle";
+import mealSvg from "../../assets/meal2.svg";
+
+const Login = ({ setIsAuth, isAuth }) => {
+  let navigate = useNavigate();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setIsAuth(true);
+    navigate("/");
+  };
+  return (
+    <LoginContainer>
+      <FormContainer>
+        <StyledImg src={mealSvg} alt="meal" />
+        <Header>{"<ed8en/>"} Recipe</Header>
+        <StyledForm onSubmit={handleSubmit}>
+          <StyledInput type="text" placeholder="Username" required />
+          <StyledInput type="password" placeholder="Password" required />
+          <StyledButton type="submit">Login</StyledButton>
+        </StyledForm>
+      </FormContainer>
+    </LoginContainer>
+  );
+};
+
+export default Login;
