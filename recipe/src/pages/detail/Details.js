62c7234f7e734b6d79722b7c0c2e57c77a4bade4
veyseltosun <postalarim1923@gmail.com>
2022-09-06 00:00:43 +0300

    location

--- recipe/src/pages/detail/Details.js
+++ recipe/src/pages/detail/Details.js
@@ -1,4 +1,5 @@
 import React from "react";
+
 import {
   DetailContainer,
   DetailPart,
@@ -8,52 +9,54 @@ import {
   OtherPart,
 } from "./DetailsStyle";
 import dietSvg from "../../assets/diet.svg";
+import { useLocation } from "react-router-dom";
 
-const Details = (props) => {
-  const recipe = props.location.recipe;
+const Details = () => {
+  const location  = useLocation();
+  console.log("recipe", location)
 
   return (
     <DetailContainer>
       <HeaderContainer>
-        <h1>{recipe.label}</h1>
+        <h1>{location.label}</h1>
         <img src={dietSvg} alt="" />
       </HeaderContainer>
       <DetailPart>
         <OtherPart>
           <>Nutrients</>
           <p>
-            {recipe.totalNutrients.CA.label} :{" "}
-            {Math.round(recipe.totalNutrients.CA.quantity)}
-            {recipe.totalNutrients.CA.unit}
+            {location.totalNutrients.CA.label} :{" "}
+            {Math.round(location.totalNutrients.CA.quantity)}
+            {location.totalNutrients.CA.unit}
           </p>
           <p>
-            {recipe.totalNutrients.CHOCDF.label} :{" "}
-            {Math.round(recipe.totalNutrients.CHOCDF.quantity)}
-            {recipe.totalNutrients.CHOCDF.unit}
+            {location.totalNutrients.CHOCDF.label} :{" "}
+            {Math.round(location.totalNutrients.CHOCDF.quantity)}
+            {location.totalNutrients.CHOCDF.unit}
           </p>
           <p>
-            {recipe.totalNutrients.CHOLE.label} :{" "}
-            {Math.round(recipe.totalNutrients.CHOLE.quantity)}
-            {recipe.totalNutrients.CHOLE.unit}
+            {location.totalNutrients.CHOLE.label} :{" "}
+            {Math.round(location.totalNutrients.CHOLE.quantity)}
+            {location.totalNutrients.CHOLE.unit}
           </p>
           <p>
-            {recipe.totalNutrients.ENERC_KCAL.label} :{" "}
-            {Math.round(recipe.totalNutrients.ENERC_KCAL.quantity)}
-            {recipe.totalNutrients.ENERC_KCAL.unit}
+            {location.totalNutrients.ENERC_KCAL.label} :{" "}
+            {Math.round(location.totalNutrients.ENERC_KCAL.quantity)}
+            {location.totalNutrients.ENERC_KCAL.unit}
           </p>
-          <p>{recipe.totalWeight}</p>
-          <p>Calories: {Math.round(recipe.calories)}</p>
-          {recipe.digest.slice(0, 4).map((item, index) => (
+          <p>{location.totalWeight}</p>
+          <p>Calories: {Math.round(location.calories)}</p>
+          {location.digest.slice(0, 4).map((item, index) => (
             <p key={index}>
               {item.label} : {Math.round(item.total)}
             </p>
           ))}
         </OtherPart>
         <ImgContainer>
-          <img src={recipe.image} alt={recipe.label} />
+          <img src={location.image} alt={location.label} />
         </ImgContainer>
         <IngContainer>
-          {recipe.ingredientLines.map((line, index) => (
+          {location.ingredientLines.map((line, index) => (
             <div key={index}>
               <p>
                 {index + 1} - {line}
